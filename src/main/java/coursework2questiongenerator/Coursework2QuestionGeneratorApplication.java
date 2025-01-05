becafd5652aaf094f822905fa8422fa2bc9b81ed
vachg <vachgen@yandex.ru>
2025-01-05 19:20:50 +0300

    Start

---
+++ src/main/java/coursework2questiongenerator/Coursework2QuestionGeneratorApplication.java
@@ -0,0 +1,13 @@
+package coursework2questiongenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework2QuestionGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursework2QuestionGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coursework2questiongenerator/Coursework2QuestionGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package coursework2questiongenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework2QuestionGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
